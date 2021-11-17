f6c697731800a53181f3616f13a53bf91ccbb8f3
lucas-cq <lucas.cummings024@gmail.com>
2021-11-16 19:22:00 -0700

    another nother test

--- pages/about.vue
+++ pages/about.vue
@@ -2,8 +2,29 @@
 
 <template>
   <div>
-    <GlobalNav/>
-    <AboutUs/>
+    <GlobalNav />
+    <AboutUs />
     <MyFooter />
   </div>
 </template>
+
+<style>
+header {
+  background-color: #ff335f;
+  margin: 0px auto;
+}
+
+body {
+  margin: 0px auto;
+}
+
+footer {
+  background-color: #ff335f;
+  padding: 20px;
+  color: white;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
